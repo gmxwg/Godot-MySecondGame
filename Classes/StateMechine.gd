3f5c05ab3daa9b28a00985907421fac5edc94e61
gmxwg <2749964659@qq.com>
2026-06-19 19:39:59 +0800

    3.feat: 新增蹬墙跳功能

--- Classes/StateMechine.gd
+++ Classes/StateMechine.gd
@@ -6,6 +6,9 @@ var CurrentState : int = -1:
 	set(v):
 		owner.transition_state(CurrentState, v)
 		CurrentState = v
+		StateTime = 0
+
+var StateTime : float
 
 func _ready() -> void:
 	await owner.ready
@@ -19,4 +22,5 @@ func _physics_process(delta: float) -> void:
 		else:
 			CurrentState = next
 
-	owner.tick_physics(CurrentState,delta)
+	owner.tick_physics(CurrentState,delta) # 状态稳定后执行对应动作
+	StateTime += delta
